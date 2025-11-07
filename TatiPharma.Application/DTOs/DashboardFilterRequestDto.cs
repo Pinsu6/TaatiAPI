30e0a1fd5c58fc32268875bf419d46d6283b4916
Prince Bhindi <23030501008@darshan.ac.in>
2025-11-07 15:28:27 +0530

    ee

---
+++ TatiPharma.Application/DTOs/DashboardFilterRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TatiPharma.Application.DTOs
+{
+    public class DashboardFilterRequestDto
+    {
+        public string? Search { get; set; }
+        public string? Region { get; set; }
+        public string? ProductCategory { get; set; }
+        public string Period { get; set; } = "Last30Days"; // Last7Days, Last90Days, etc.
+    }
+}
---
+++ TatiPharma.Application/DTOs/DashboardKpiDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TatiPharma.Application.DTOs
+{
+    public class DashboardKpiDto
+    {
+        public decimal TotalSalesYtd { get; set; }
+        public decimal SalesGrowth { get; set; }
+        public int ActivePharmacies { get; set; }
+        public int TotalCustomers { get; set; }
+        public decimal OrderFulfillment { get; set; }
+        public decimal FulfillmentChange { get; set; }
+    }
+}
